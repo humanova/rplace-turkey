2f03d971dc7187c88a7f369b8f38b435aae73a47
humanova <craxes_humanova@hotmail.com>
2022-04-02 21:49:50 +0300

    add bayrak2

--- src/generate_static_ui.py
+++ src/generate_static_ui.py
@@ -85,6 +85,10 @@ if __name__ == '__main__':
                             output_filename="index.html",
                             top_left=(299,343))
 
+    generate_static_webpage(image_path="../img/turkish_flag2.png",
+                        output_filename="bayrak2.html",
+                        top_left=(299,343))
+
     generate_static_webpage(image_path="../img/chad.png",
                         output_filename="zade.html",
                         top_left=(480,414))
