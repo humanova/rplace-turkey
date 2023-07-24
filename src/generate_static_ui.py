87d0519ea09efee23ef4720c728031f125ad95ae
humanova <craxes_humanova@hotmail.com>
2023-07-24 16:42:42 +0300

    update bayrak

--- src/generate_static_ui.py
+++ src/generate_static_ui.py
@@ -78,9 +78,9 @@ def generate_static_webpage(image_path: str, output_filename:str, top_left:tuple
 
 
 if __name__ == '__main__':
-    generate_static_webpage(image_path="../img/topright_ataturk2.png",
-                            output_filename="ataturk.html",
-                            top_left=(1000, 0))
+    generate_static_webpage(image_path="../img/bayrak2023.png",
+                            output_filename="bayrak2023.html",
+                            top_left=(-368, 290))
 
     generate_static_webpage(image_path="../img/turkishcat.png",
             output_filename="turkishcat.html",
